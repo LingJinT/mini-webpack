4e1ebfa6fa34f004f0fd299a7580e34a382c0765
ImDaret <969122484@qq.com>
2023-01-01 16:24:30 +0800

    plugin finish

--- example/dist/bundle.js
+++ example/dist/bundle.js
@@ -1,26 +1,26 @@
 (function (modules) {
-    function require(id) {
-      const [mapping, fn] = modules[id]
-
-      function _require(path) {
-        return require(mapping[path])
-      }
-
-      const module = {exports:{}}
-
-      fn(_require, module, module.exports)
-
-      return module.exports
-    }
-    require(0)
-  })({0: [{"./foo/foo.js":1}, function(require, module, exports) {
-        "use strict";
+			function require(id) {
+				const [mapping, fn] = modules[id]
+	
+				function _require(path) {
+					return require(mapping[path])
+				}
+	
+				const module = {exports:{}}
+	
+				fn(_require, module, module.exports)
+	
+				return module.exports
+			}
+			require(0)
+		})({0: [{"./foo/foo.js":1}, function(require, module, exports) {
+						"use strict";
 
 var _foo = require("./foo/foo.js");
 console.log("mini-webpack");
 (0, _foo.foo)();
-      }],1: [{"../bar.js":2}, function(require, module, exports) {
-        "use strict";
+					}],1: [{"../bar.js":2}, function(require, module, exports) {
+						"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -31,8 +31,8 @@ function foo() {
   console.log("foo");
 }
 (0, _bar.bar)();
-      }],2: [{}, function(require, module, exports) {
-        "use strict";
+					}],2: [{}, function(require, module, exports) {
+						"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -41,4 +41,4 @@ exports.bar = bar;
 function bar() {
   console.log('bar');
 }
-      }],})
+					}],})
